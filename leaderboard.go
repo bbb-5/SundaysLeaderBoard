54bdbb6f605b92d036c7929af2dec58595f358df
bbb-5 <aliisa.torkki@kolumbus.fi>
2025-05-23 09:45:10 +0300

    finds participation amount

--- leaderboard.go
+++ leaderboard.go
@@ -9,8 +9,6 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-//const db_file string = "SundaysDatabase.db"
-
 type Player struct {
 	Name      string
 	Player_Id int
@@ -51,6 +49,34 @@ func find_players(path string) []*Player {
 	return players
 }
 
+//------- Finding how many tournaments certain player has attended -------------------------
+
+func find_participation(player *Player, path string) int {
+
+	participated := []int{}
+
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	participations, err := db.Query("SELECT COUNT(player_id) FROM PlayerTeam WHERE player_id = ?", player.Player_Id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for participations.Next() {
+		var amount int
+		participations.Scan(&amount)
+		participated = append(participated, amount)
+	}
+
+	db.Close()
+	return participated[0]
+}
+
+//-------Getting a player's wins-------------------------
+
 func main() {
 
 	db_argument := os.Args[1]
@@ -58,5 +84,8 @@ func main() {
 
 	players := find_players(db_argument)
 	print(players)
+
+	participation := find_participation(players[2], db_argument)
+	print("\n", players[2].Name, " has participatied ", participation, " times")
 
 }
