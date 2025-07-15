92f4e1a1c743609e19ecc2db57c4324f06428dc5
bbb-5 <aliisa.torkki@kolumbus.fi>
2025-07-15 14:27:31 +0300

    radio buttons

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,11 +1,25 @@
 import Button from "./Button"
 
 const TopBar = ({reverseHandler, filterHandler}) => {
+    
+    /*
     return (
         <div>
             <Button onClick={() => filterHandler('Indoor')} label='Indoor'> </Button>
             <Button onClick={() => filterHandler('Beach')} label='Beach'> </Button>
             <Button label='Both'> </Button>
+            <Button onClick={reverseHandler} label='Reverse'> </Button>
+        </div>
+    )*/
+
+    return (
+        <div>
+            <input type="radio" id="indoor" name="tournament_type"></input>
+            <label>Indoor</label>
+            <input type="radio" id="beach" name="tournament_type"></input>
+            <label>Beach</label>
+            <input type="radio" id="both" name="tournament_type" defaultChecked></input>
+            <label>Both</label>
             <Button onClick={reverseHandler} label='Reverse'> </Button>
         </div>
     )
