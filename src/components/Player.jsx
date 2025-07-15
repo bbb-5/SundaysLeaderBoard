859d446bbe00ecd765a79b3ef49b9e199b3f2f6e
bbb-5 <aliisa.torkki@kolumbus.fi>
2025-07-15 13:26:53 +0300

    percentages

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,7 +7,7 @@ const Player = ({player}) => {
             <p>Silver: {player.silver}</p>
             <p>Bronze: {player.bronze}</p>
             <p>Participation: {player.participation}</p>
-            <p>Winning Ratio: {(player.gold / player.participation/100)*100}</p>
+            <p>Winning Ratio: {((player.gold / player.participation)*100).toFixed(2)+'%'}</p>
             <p>Medals: {player.gold+player.silver+player.bronze}</p>
             <p>Extra Awards: {player.extra_awards.length}</p>
         </div>
