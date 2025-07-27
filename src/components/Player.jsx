fb4a4cc36071d321bbbf37cd91412edc71a1aa66
bbb-5 <aliisa.torkki@kolumbus.fi>
2025-07-27 21:19:50 +0300

    player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,17 +7,27 @@ const Player = ({player}) => {
     return (
         <div>
             <p>{player.name}</p>
-            <p>Gold: {player.gold}</p>
-            <p>Silver: {player.silver}</p>
-            <p>Bronze: {player.bronze}</p>
             <p>Participation Indoor: {player.participation_indoor}</p>
             <p>Participation Beach: {player.participation_beach}</p>
-            <p>Winning Ratio Indoor: {((player.placements.filter((placement) => (placement.medaltype === 'Gold_Indoor')).length / player.participation_indoor)*100).toFixed(2)+'%'}</p>
-            <p>Winning Ratio Beach: {((player.placements.filter((placement) => (placement.medaltype === 'Gold_Beach')).length / player.participation_beach)*100).toFixed(2)+'%'}</p>
-            <p>Medals: {player.gold+player.silver+player.bronze}</p>
             <p>Extra Awards: {player.extra_awards.length}</p>
         </div>
     )
 }
 
 export default Player
+
+/*
+return (
+    <div>
+        <p>{player.name}</p>
+        <p>Gold: {player.gold}</p>
+        <p>Silver: {player.silver}</p>
+        <p>Bronze: {player.bronze}</p>
+        <p>Participation Indoor: {player.participation_indoor}</p>
+        <p>Participation Beach: {player.participation_beach}</p>
+        <p>Winning Ratio Indoor: {((player.placements.filter((placement) => (placement.medaltype === 'Gold_Indoor')).length / player.participation_indoor)*100).toFixed(2)+'%'}</p>
+        <p>Winning Ratio Beach: {((player.placements.filter((placement) => (placement.medaltype === 'Gold_Beach')).length / player.participation_beach)*100).toFixed(2)+'%'}</p>
+        <p>Medals: {player.gold+player.silver+player.bronze}</p>
+        <p>Extra Awards: {player.extra_awards.length}</p>
+    </div>
+)*/
