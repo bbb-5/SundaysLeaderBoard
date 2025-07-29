ca35329297582b87787add99830484ed70620f37
bbb-5 <aliisa.torkki@kolumbus.fi>
2025-07-29 22:46:50 +0300

    both ratio, buttons rework

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -6,20 +6,48 @@ const BottomBar = ({sortHandler, filter_by, players}) => {
         return () => sortHandler(func)
     }
 
+    const ratio = (a,b,filter_by) => {
+
+        console.log(filter_by)
+        console.log('Made it')
+
+        let b_ratio = 0
+        let a_ratio = 0
+
+        
+        switch(filter_by){
+            case "Beach":
+                console.log('There')   
+                b_ratio = (b.placements.filter((placement) => (placement.medaltype.medal==='Gold'))).length/(b.participation_beach)
+                a_ratio = (a.placements.filter((placement) => (placement.medaltype.medal==='Gold'))).length/(a.participation_beach)
+                return (b_ratio - a_ratio)
+            
+            case "Indoor":
+                console.log('Now')   
+                b_ratio = ((b.placements.filter((placement) => (placement.medaltype.medal==='Gold'))).length/(b.participation_indoor))
+                a_ratio = ((a.placements.filter((placement) => (placement.medaltype.medal==='Gold'))).length/(a.participation_indoor))
+                return (b_ratio - a_ratio)
+            
+            default: 
+                console.log('Here')
+                b_ratio = ((b.placements.filter((placement) => (placement.medaltype.medal==='Gold'))).length/(b.participation_indoor+b.participation_beach))
+                a_ratio = ((a.placements.filter((placement) => (placement.medaltype.medal==='Gold'))).length/(a.participation_indoor+a.participation_beach))
+                return (b_ratio - a_ratio)
+        }
+    }
+
     return (
         <div>
-            <Button onClick={sort((a, b) => (b.placements.filter((placement) => (placement.medaltype.location === 'Indoor' && placement.medaltype.medal ==='Gold'))) - (a.placements.filter((placement) => (placement.medaltype.location === 'Indoor' && placement.medaltype.medal ==='Gold'))))} label='Gold'> </Button>
-            <Button onClick={sort((a, b) => b.silver - a.silver)} label='Silver'> </Button>
-            <Button onClick={sort((a, b) => b.bronze - a.bronze)} label='Bronze'> </Button>
-            <Button onClick={sort((a, b) => (b.gold/b.participation) - (a.gold/a.participation))} label='Percentage'> </Button>
+            <Button onClick={sort((a, b) => (b.placements.filter((placement) => (placement.medaltype.medal ==='Gold'))).length - (a.placements.filter((placement) => (placement.medaltype.medal ==='Gold'))).length)} label='Gold'> </Button>
+            <Button onClick={sort((a, b) => (b.placements.filter((placement) => (placement.medaltype.medal ==='Silver'))).length - (a.placements.filter((placement) => (placement.medaltype.medal ==='Silver'))).length)} label='Silver'> </Button>
+            <Button onClick={sort((a, b) => (b.placements.filter((placement) => (placement.medaltype.medal ==='Bronze'))).length - (a.placements.filter((placement) => (placement.medaltype.medal ==='Bronze'))).length)} label='Bronze'> </Button>
+
+            <Button onClick={sort((a, b) => ratio(a,b,filter_by))} label='Percentage'> </Button>
 
             <Button onClick={sort((a, b) => b.placements.length - a.placements.length)} label='Over all'> </Button>
             <Button onClick={sort((a, b) => b.extra_awards.length - a.extra_awards.length)} label='Extra voting'> </Button>
         </div>
     )
 }
-
-
-//(player.placements.filter((placement) => (placement.medaltype.location === 'Indoor' && placement.medaltype.medal ==='Gold')
 
 export default BottomBar
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -11,6 +11,7 @@ const Player = ({player}) => {
             <p>Participation Beach: {player.participation_beach}</p>
             <p>Winning Ratio Indoor: {((player.placements.filter((placement) => (placement.medaltype.location === 'Indoor' && placement.medaltype.medal ==='Gold')).length / player.participation_indoor)*100).toFixed(2)+'%'}</p>
             <p>Winning Ratio Beach: {((player.placements.filter((placement) => (placement.medaltype.location === 'Beach' && placement.medaltype.medal ==='Gold')).length / player.participation_beach)*100).toFixed(2)+'%'}</p>
+            <p>Winning Ratio Both: {(player.placements.filter((placement) => (placement.medaltype.medal ==='Gold')).length / (player.participation_beach+player.participation_indoor)*100).toFixed(2)+'%'}</p>
             <p>Extra Awards: {player.extra_awards.length}</p>
         </div>
     )
