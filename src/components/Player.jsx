7c1da921ef12d025c80acaf4eef2978f5732a924
bbb-5 <aliisa.torkki@kolumbus.fi>
2025-07-30 14:10:41 +0300

    percentage function

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,7 @@
 const Player = ({player}) => {
 
+    const percen = (n) => (n * 100).toFixed(2) + '%'
+
     return (
         <div>
             <p>{player.name}</p>
@@ -9,14 +11,12 @@ const Player = ({player}) => {
             <p>Medals: {player.placements.length}</p>
             <p>Participation Indoor: {player.participation_indoor}</p>
             <p>Participation Beach: {player.participation_beach}</p>
-            <p>Winning Ratio Indoor: {((player.placements.filter((placement) => (placement.medaltype.location === 'Indoor' && placement.medaltype.medal ==='Gold')).length / player.participation_indoor) * 100).toFixed(2) + '%'}</p>
-            <p>Winning Ratio Beach: {((player.placements.filter((placement) => (placement.medaltype.location === 'Beach' && placement.medaltype.medal ==='Gold')).length / player.participation_beach) * 100).toFixed(2) + '%'}</p>
-            <p>Winning Ratio Both: {(player.placements.filter((placement) => (placement.medaltype.medal ==='Gold')).length / (player.participation_beach + player.participation_indoor) * 100).toFixed(2) + '%'}</p>
+            <p>Winning Ratio Indoor: {percen(player.placements.filter((placement) => (placement.medaltype.location === 'Indoor' && placement.medaltype.medal ==='Gold')).length / player.participation_indoor)}</p>
+            <p>Winning Ratio Beach: {percen(player.placements.filter((placement) => (placement.medaltype.location === 'Beach' && placement.medaltype.medal ==='Gold')).length / player.participation_beach)}</p>
+            <p>Winning Ratio Both: {percen(player.placements.filter((placement) => (placement.medaltype.medal ==='Gold')).length / (player.participation_beach + player.participation_indoor))}</p>
             <p>Extra Awards: {player.extra_awards.length}</p>
         </div>
     )
 }
-
-const percen = (n) => (n * 100).toFixed(2) + '%'
 
 export default Player
