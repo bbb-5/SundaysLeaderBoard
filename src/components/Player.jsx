3c5d4c273c49833bc7a82bd0d9fced479fa23767
bbb-5 <aliisa.torkki@kolumbus.fi>
2025-08-11 13:24:57 +0300

    no nans string

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,6 +2,30 @@ const Player = ({player}) => {
 
     const percen = (n) => (n * 100).toFixed(2) + '%'
 
+    const ratio = (player, filter) => {
+        switch(filter){
+            case 'Indoor':
+                if (player.participation_indoor === 0){
+                    return 0
+                } else {
+                    return (player.placements.filter((placement) => (placement.medaltype.location === 'Indoor' && placement.medaltype.medal ==='Gold')).length / player.participation_indoor)
+                }
+            case 'Beach':
+                if (player.participation_beach === 0){
+                    return 0
+                } else {
+                    return (player.placements.filter((placement) => (placement.medaltype.location === 'Beach' && placement.medaltype.medal ==='Gold')).length / player.participation_beach)
+                }
+        
+            default:
+                if (player.participation_beach === 0){
+                    return 0
+                } else {
+                    return (player.placements.filter((placement) => (placement.medaltype.medal ==='Gold')).length / (player.participation_beach + player.participation_indoor))
+                }
+        }
+    }
+
     return (
         <div>
             <p>{player.name}</p>
@@ -11,12 +35,16 @@ const Player = ({player}) => {
             <p>Medals: {player.placements.length}</p>
             <p>Participation Indoor: {player.participation_indoor}</p>
             <p>Participation Beach: {player.participation_beach}</p>
-            <p>Winning Ratio Indoor: {percen(player.placements.filter((placement) => (placement.medaltype.location === 'Indoor' && placement.medaltype.medal ==='Gold')).length / player.participation_indoor)}</p>
-            <p>Winning Ratio Beach: {percen(player.placements.filter((placement) => (placement.medaltype.location === 'Beach' && placement.medaltype.medal ==='Gold')).length / player.participation_beach)}</p>
-            <p>Winning Ratio Both: {percen(player.placements.filter((placement) => (placement.medaltype.medal ==='Gold')).length / (player.participation_beach + player.participation_indoor))}</p>
+            <p>Winning Ratio Indoor: {percen(ratio(player,'Indoor'))}</p>
+            <p>Winning Ratio Beach: {percen(ratio(player,'Beach'))}</p>
+            <p>Winning Ratio Both: {percen(ratio(player,'Both'))}</p>
             <p>Extra Awards: {player.extra_awards.length}</p>
         </div>
     )
 }
 
 export default Player
+
+//<p>Winning Ratio Indoor: {percen(player.placements.filter((placement) => (placement.medaltype.location === 'Indoor' && placement.medaltype.medal ==='Gold')).length / player.participation_indoor)}</p>
+//<p>Winning Ratio Beach: {percen(player.placements.filter((placement) => (placement.medaltype.location === 'Beach' && placement.medaltype.medal ==='Gold')).length / player.participation_beach)}</p>
+//<p>Winning Ratio Both: {percen(player.placements.filter((placement) => (placement.medaltype.medal ==='Gold')).length / (player.participation_beach + player.participation_indoor))}</p>
