0558a28a3a87a2a6c5d394b826f4bf45579bb62c
bbb-5 <aliisa.torkki@kolumbus.fi>
2025-08-11 16:48:16 +0300

    bottom refactor

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -1,51 +1,27 @@
+import { useState } from "react"
 import Button from "./Button"
 
-const BottomBar = ({sortHandler, filter_by, players}) => {
+const BottomBar = ({sortHandler, filter_by, handleSelected, sort_by}) => {
 
-    const sort = (func) => {
-        return () => sortHandler(func)
-    }
-
-    const ratio = (a,b,filter_by) => {
-
-        console.log(filter_by)
-        console.log('Made it')
-
-        let b_ratio = 0
-        let a_ratio = 0
-
-        
-        switch(filter_by){
-            case "Beach":
-                console.log('There')
-                b_ratio = (b.placements.filter((placement) => (placement.medaltype.medal==='Gold'))).length/(b.participation_beach)
-                a_ratio = (a.placements.filter((placement) => (placement.medaltype.medal==='Gold'))).length/(a.participation_beach)
-                return (b_ratio - a_ratio)
-            
-            case "Indoor":
-                console.log('Now')
-                b_ratio = ((b.placements.filter((placement) => (placement.medaltype.medal==='Gold'))).length/(b.participation_indoor))
-                a_ratio = ((a.placements.filter((placement) => (placement.medaltype.medal==='Gold'))).length/(a.participation_indoor))
-                return (b_ratio - a_ratio)
-            
-            default: 
-                console.log('Here')
-                b_ratio = ((b.placements.filter((placement) => (placement.medaltype.medal==='Gold'))).length/(b.participation_indoor+b.participation_beach))
-                a_ratio = ((a.placements.filter((placement) => (placement.medaltype.medal==='Gold'))).length/(a.participation_indoor+a.participation_beach))
-                return (b_ratio - a_ratio)
-        }
-    }
+    
+    const Selected = {
+        Gold: "Gold",
+        Silver: "Silver",
+        Bronze: "Bronze",
+        Percentage: "Percentage",
+        Total: "Over all",
+        Extra: "Extra Award"
+    } 
 
     return (
         <div>
-            <Button onClick={sort((a, b) => (b.placements.filter((placement) => (placement.medaltype.medal ==='Gold'))).length - (a.placements.filter((placement) => (placement.medaltype.medal ==='Gold'))).length)} label='Gold'> </Button>
-            <Button onClick={sort((a, b) => (b.placements.filter((placement) => (placement.medaltype.medal ==='Silver'))).length - (a.placements.filter((placement) => (placement.medaltype.medal ==='Silver'))).length)} label='Silver'> </Button>
-            <Button onClick={sort((a, b) => (b.placements.filter((placement) => (placement.medaltype.medal ==='Bronze'))).length - (a.placements.filter((placement) => (placement.medaltype.medal ==='Bronze'))).length)} label='Bronze'> </Button>
 
-            <Button onClick={sort((a, b) => ratio(a,b,filter_by))} label='Percentage'> </Button>
+            <input type="radio" id="gold" name="icon" value="Gold" checked={sort_by === Selected.Gold} onChange={(e) => handleSelected(e)}></input>
+            <label>Gold</label>
 
-            <Button onClick={sort((a, b) => b.placements.length - a.placements.length)} label='Over all'> </Button>
-            <Button onClick={sort((a, b) => b.extra_awards.length - a.extra_awards.length)} label='Extra voting'> </Button>
+            <input type="radio" id="silver" name="icon" value="Silver" checked={sort_by === Selected.Silver} onChange={(e) => handleSelected(e)}></input>
+            <label>Silver</label>
+
         </div>
     )
 }
