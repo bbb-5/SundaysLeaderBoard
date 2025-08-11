c34d35f18ec56a0ebf465846993073dfc0e65b8e
bbb-5 <aliisa.torkki@kolumbus.fi>
2025-08-11 13:33:41 +0300

    no nan enum

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,16 +1,22 @@
 const Player = ({player}) => {
 
+    const Filters = {
+        Indoor: "Indoor",
+        Beach: "Beach",
+        Both: "Both"
+    }
+
     const percen = (n) => (n * 100).toFixed(2) + '%'
 
     const ratio = (player, filter) => {
         switch(filter){
-            case 'Indoor':
+            case Filters.Indoor:
                 if (player.participation_indoor === 0){
                     return 0
                 } else {
                     return (player.placements.filter((placement) => (placement.medaltype.location === 'Indoor' && placement.medaltype.medal ==='Gold')).length / player.participation_indoor)
                 }
-            case 'Beach':
+            case Filters.Beach:
                 if (player.participation_beach === 0){
                     return 0
                 } else {
@@ -35,9 +41,9 @@ const Player = ({player}) => {
             <p>Medals: {player.placements.length}</p>
             <p>Participation Indoor: {player.participation_indoor}</p>
             <p>Participation Beach: {player.participation_beach}</p>
-            <p>Winning Ratio Indoor: {percen(ratio(player,'Indoor'))}</p>
-            <p>Winning Ratio Beach: {percen(ratio(player,'Beach'))}</p>
-            <p>Winning Ratio Both: {percen(ratio(player,'Both'))}</p>
+            <p>Winning Ratio Indoor: {percen(ratio(player,Filters.Indoor))}</p>
+            <p>Winning Ratio Beach: {percen(ratio(player,Filters.Beach))}</p>
+            <p>Winning Ratio Both: {percen(ratio(player,Filters.Both))}</p>
             <p>Extra Awards: {player.extra_awards.length}</p>
         </div>
     )
