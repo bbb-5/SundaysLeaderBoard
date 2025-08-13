1b68143a2e8f9b1d9c854894a42fa199c8cabb17
bbb-5 <aliisa.torkki@kolumbus.fi>
2025-08-13 22:45:35 +0300

    rm leaderboard directory

--- leaderboard/src/services/players.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const baseUrl = './players.json'
-
-const getPlayers = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response)
-}
-
-export default {getPlayers}
