987f4db6e8a756e96aed6f0852123e4d935c1b2d
bbb-5 <aliisa.torkki@kolumbus.fi>
2026-02-18 21:37:43 +0200

    temp icons for topbar

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ label, onClick, selected }) => {
-    return (
-        <button className={true ? "checked" : "notChecked"} onClick={onClick} selected={selected}>
-            {label}
-        </button>
-    )
-}
-
-export default Button
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,19 +1,29 @@
 import Calendar from "./Calendar"
-import Button from "./Button"
 
 const TopBar = ({ reverseHandler, filterHandler, filter_by, tournaments, onStartDate, onEndDate }) => {
 
     return (
-        <div>
-            <Calendar tournaments={tournaments} default_idx={0} onDatesSelected={onStartDate} filter={filter_by} />
+        <div className="topbar">
+            <Calendar className="calendar" tournaments={tournaments} default_idx={0} onDatesSelected={onStartDate} filter={filter_by} />
             <Calendar tournaments={tournaments} default_idx={tournaments.length - 1} onDatesSelected={onEndDate} filter={filter_by} />
-            <input type="radio" id="indoor" name="tournament_type" value="Indoor" checked={filter_by === "Indoor"} onChange={(e) => filterHandler(e)}></input>
-            <label>Indoor</label>
-            <input type="radio" id="beach" name="tournament_type" value="Beach" checked={filter_by === "Beach"} onChange={(e) => filterHandler(e)}></input>
-            <label>Beach</label>
-            <input type="radio" id="both" name="tournament_type" value="Both" checked={filter_by === "Both"} onChange={(e) => filterHandler(e)}></input>
-            <label>Both</label>
-            <Button onClick={reverseHandler} label='Reverse' />
+
+            <label>
+                <input type="radio" className="filter" id="indoor" name="tournament_type" value="Indoor" checked={filter_by === "Indoor"} onChange={(e) => filterHandler(e)}></input>
+                <img src="./public/icons/ICONS2_TIndoor.svg" width={55} height={55} />
+            </label>
+            
+            <label>
+                <input type="radio" className="filter" id="beach" name="tournament_type" value="Beach" checked={filter_by === "Beach"} onChange={(e) => filterHandler(e)}></input>
+                <img src="./public/icons/ICONS2_TBeach.svg" width={55} height={55} />
+            </label>
+
+            <label>
+                <input type="radio" className="filter" id="both" name="tournament_type" value="Both" checked={filter_by === "Both"} onChange={(e) => filterHandler(e)}></input>
+                <img src="./public/icons/ICONS2_TBeach - Indoor.svg" width={55} height={55} />
+            </label>
+
+            <img className="reverse" src="./public/icons/ICONS2_Reverse.svg" onClick={reverseHandler} width={55} height={55}/>
+            
         </div>
     )
 }
