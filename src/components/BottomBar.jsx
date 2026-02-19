65ae43db590d0ec8990378e270f81a6c9eaa8270
bbb-5 <aliisa.torkki@kolumbus.fi>
2026-02-20 01:05:02 +0200

    deploy

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -21,27 +21,27 @@ const BottomBar = ({ handleSelected, sort_by, filter_by }) => {
             <div>
                 <label>
                     <input type="radio" className="sorter" id="gold" name="icon" value="Gold" checked={sort_by === Selected.Gold} onChange={(e) => handleSelected(e)}></input>
-                    <img src="./public/icons/ICONS2_Gold.svg" width={55} height={55} />
+                    <img src="icons/ICONS2_Gold.svg" width={55} height={55} />
                 </label>
 
                 <label>
                     <input type="radio" className="sorter" id="percentage" name="icon" value="Percentage" checked={sort_by === Selected.Percentage} onChange={(e) => handleSelected(e)}></input>
-                    <img src="./public/icons/ICONS2_Winning percentage (WP).svg" width={55} height={55} />
+                    <img src="icons/ICONS2_Winning percentage (WP).svg" width={55} height={55} />
                 </label>
 
                 <label>
                     <input type="radio" className="sorter" id="total" name="icon" value="Total" checked={sort_by === Selected.Total} onChange={(e) => handleSelected(e)}></input>
-                    <img src="./public/icons/ICONS2_Total amount of medals.svg" width={55} height={55} />
+                    <img src="icons/ICONS2_Total amount of medals.svg" width={55} height={55} />
                 </label>
 
                 <label>
                     <input type="radio" className="sorter" id="extra" name="icon" value="Extra" checked={sort_by === Selected.Extra} onChange={(e) => handleSelected(e)}></input>
-                    <img src="./public/icons/ICONS2_Extra voting topic.svg" width={55} height={55} />
+                    <img src="icons/ICONS2_Extra voting topic.svg" width={55} height={55} />
                 </label>
 
                 <label>
                     <input type="radio" className="sorter" id="default" name="icon" value="Default" checked={sort_by === Selected.Default} onChange={(e) => handleSelected(e)}></input>
-                    <img src="./public/icons/ICONS2_Refresh.svg" width={55} height={55} />
+                    <img src="icons/ICONS2_Refresh.svg" width={55} height={55} />
                 </label>
 
             </div>
@@ -52,22 +52,22 @@ const BottomBar = ({ handleSelected, sort_by, filter_by }) => {
         <div>
             <label>
                 <input type="radio" className="sorter" id="gold" name="icon" value="Gold" checked={sort_by === Selected.Gold} onChange={(e) => handleSelected(e)}></input>
-                <img src="./public/icons/ICONS2_Gold.svg" width={55} height={55} />
+                <img src="/icons/ICONS2_Gold.svg" width={55} height={55} />
             </label>
             
             <label>
                 <input type="radio" className="sorter" id="silver" name="icon" value="Silver" checked={sort_by === Selected.Silver} onChange={(e) => handleSelected(e)}></input>
-                <img src="./public/icons/ICONS2_Silver.svg" width={55} height={55} />
+                <img src="icons/ICONS2_Silver.svg" width={55} height={55} />
             </label>
 
             <label>
                 <input type="radio" className="sorter" id="bronze" name="icon" value="Bronze" checked={sort_by === Selected.Bronze} onChange={(e) => handleSelected(e)}></input>
-                <img src="./public/icons/ICONS2_Bronze.svg" width={55} height={55} />
+                <img src="icons/ICONS2_Bronze.svg" width={55} height={55} />
             </label>
 
             <label>
                 <input type="radio" className="sorter" id="percentage" name="icon" value="Percentage" checked={sort_by === Selected.Percentage} onChange={(e) => handleSelected(e)}></input>
-                <img src="./public/icons/ICONS2_Winning percentage (WP).svg" width={55} height={55} />
+                <img src="icons/ICONS2_Winning percentage (WP).svg" width={55} height={55} />
             </label>
 
             <label>
@@ -77,12 +77,12 @@ const BottomBar = ({ handleSelected, sort_by, filter_by }) => {
 
             <label>
                 <input type="radio" className="sorter" id="extra" name="icon" value="Extra" checked={sort_by === Selected.Extra} onChange={(e) => handleSelected(e)}></input>
-                <img src="./public/icons/ICONS2_Extra voting topic.svg" width={55} height={55} />
+                <img src="icons/ICONS2_Extra voting topic.svg" width={55} height={55} />
             </label>
 
             <label>
                 <input type="radio" className="sorter" id="default" name="icon" value="Default" checked={sort_by === Selected.Default} onChange={(e) => handleSelected(e)}></input>
-                <img src="./public/icons/ICONS2_Refresh.svg" width={55} height={55} />
+                <img src="icons/ICONS2_Refresh.svg" width={55} height={55} />
             </label>
 
         </div>
